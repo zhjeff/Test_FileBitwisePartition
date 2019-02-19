94f16de8e8bd738d091583a85e42f9031cbb963d
zhjeff <juemin@gmail.com>
2019-02-19 15:58:41 -0500

    Initial stage the test cmake files

---
+++ test_cmake/test_cmake/test_cmake.cpp
@@ -0,0 +1,12 @@
+﻿// test_cmake.cpp : Defines the entry point for the application.
+//
+
+#include "test_cmake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
